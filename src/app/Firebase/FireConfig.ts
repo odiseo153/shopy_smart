1c141008accdee78359eaa85ee97c6c80b7727c7
OdiseoRincon@gmail.com <OdiseoRincon@gmail.com>
2025-01-14 15:47:33 -0400

    Subiendo arreglo de firebase config

--- src/app/Firebase/FireConfig.ts
+++ src/app/Firebase/FireConfig.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
-import key from  '../Global/env.ts';
+import key from  '../Global/env';
 
 
 const firebaseConfig = { 
